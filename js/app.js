64559fef972abc6520bfeb1e8f408f01747f13e4
Kevin Hu <khu@liveperson.com>
2020-08-25 11:31:16 -0400

    edit if no agent sdk hide ui

--- js/app.js
+++ js/app.js
@@ -14,13 +14,47 @@ let carousel = {
   padding: 10,
   elements: []
 };
-lpTag.agentSDK.init();
+try {
+  var notificationHandler = function(data) {
+    console.log(data);
+  };
 
-window.addEventListener("load", () => {
+  var focusHandler = function() {
+    console.log("visitor is focused");
+  };
+
+  var blurHandler = function() {
+    console.log("visitor is blurred");
+  };
+  let initiated = lpTag.agentSDK.init({
+    notificationCallback: notificationHandler,
+    visitorFocusedCallback: focusHandler,
+    visitorBlurredCallback: blurHandler
+  });
+  
+} catch (e) {
+  console.log(e);
+}
+
+window.addEventListener("load", async () => {
   console.log("hi");
   console.log(card);
   console.log(JsonPollock);
-  
+  var onSuccess = function(data) {
+    if(data) {
+    console.log("Agent SDK is initiated: ", data);
+    let body = document.getElementsByTagName("body")[0];
+    body.style.visibility = "visible";
+    }
+  };
+
+  var onError = function(err) {
+    console.log(err);
+  };
+
+  var pathToData = "visitorInfo.visitorName";
+
+  lpTag.agentSDK.get(pathToData, onSuccess, onError);
   const rooEl = JsonPollock.render(card);
   document.getElementById("inner-container").appendChild(rooEl);
 });
